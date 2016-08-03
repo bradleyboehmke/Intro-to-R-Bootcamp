a34593ba0a008f7b67daf617b595d6cb80d1f7b9
Brad Boehmke <bradleyboehmke@gmail.com>
2016-08-02 23:06:41 -0400

    case study .R files

--- Files - students/10-case-study-2.R
+++ Files - students/10-case-study-2.R
@@ -1,26 +1,26 @@
 # Download the most recent Occupational Employment Statistics data (May 2015)
-# from http://www.bls.gov/oes/tables.htm
-
-# Note: you can download the .txt file directly at http://download.bls.gov/pub/time.series/oe/oe.series
-
-
-# How does Cincinnati rank nationally for average salary across all occupations & industries?
+# from https://www.dropbox.com/s/5cagoqaa67uix3e/all_data_M_2015.xlsx?dl=0
 
 
 
-
-# How does Cincinnati rank within the state of Ohio?
+# How does Cincinnati rank nationally for median annual wage across all occupations & industries?
+# Note, you only want to compare Cincinnati to other Metropolitan Statistical Areas
 
 
 
 
-# Identify the occupations that represent the top 10 employers in Cincinnati
+# How does Cincinnati rank among the following locations for median annual wage and
+# also total employment
+
+
+
+
+# Identify the top 25 occupations that employ the most people in Cincinnati
 
 
 
 
 # Do these occupations provide the top paying salaries? If not, which occupations do?
-
 
 
 
--- Files - students/9-case-study-1.R
+++ Files - students/9-case-study-1.R
@@ -1,13 +1,16 @@
 # Download the Police Crime Incident Data at 
-# https://data.cincinnati-oh.gov/Safer-Streets/Police-Crime-Incident-Data/w7vh-beui
-
-
-# What are the top 10 most prevelant offenses in this data set?
+# https://www.dropbox.com/s/vc4ldivb2op7phl/crime.csv?dl=0
 
 
 
+# What are the top 25 most prevelant offenses in this data set? What are some
+# issues you are finding with the coding of this data?
 
-# Are the rate of occurrences for these offenses increasing or decreasing?
+
+
+
+# Have the rate of occurrence for the top 10 offenses increasing or decreasing
+# since 2010?
 
 
 
